844eaddd94e9b11599ab3e9aa73883a0fc5916b8
Ares <diego.amachiflores@gmail.com>
2024-04-30 17:12:01 -0500

    30/04/2024

--- src/components/cardfactura.tsx
+++ src/components/cardfactura.tsx
@@ -1,22 +1,62 @@
 import FileZipIcon from "../icons/filezipicon"
 import facturas from '../assets/facturas.json' 
-
+import '../styles/cards.css'
+import CheckIcon from "../icons/checkicon"
 function CardFactura() {
   
   return (
-    <div className=" bg-[#1e1e1e] w-full h-full">
-      <div className=" flex flex-col gap-2 p-2 ">
-        {facturas.map((f)=>(
-            <a className="flex w-full items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-            <FileZipIcon className="h-4"/>
-              <span className="flex-1 ms-3 whitespace-nowrap">
-               {f.numero}
-               {f.cliente}
+    <div className=" bg-[#252526] w-1/6 h-full overflow-hidden flex flex-col">
+      <section className='text-xs px-4 pt-2 h-12 text-white items-center flex font-bold'>
+          EMITIR NOTA DE CREDITO
+      </section>
+      <article className='px-4 mr-0 p-0 flex  h-12 ' >
+          {/*
+            <button className='grid items-center justify-center'>
+                <MenorIcon className='w-4'/>
+            </button>
+          */}
+          <article className=' w-full h-full flex flex-col gap-1 justify-center items-center'>
+              <form className='flex w-full h-7 '>
+                  <input type="search" placeholder="Buscar comprobante..." className=' text-sm px-2 rounded bg-[#333333] inputs focus:outline-none focus:ring-[#007acc] focus:ring-1'/>
+              </form> 
+              {/*
+              <form action="" className='flex w-full h-[22px]  '>
+                <input type="text" placeholder="Buscar..." className=' text-sm w-full pl-4 rounded'/>
+              </form> 
+              */}
+          </article>
+      </article>
+      <div className=" flex flex-col px-4">
+        {facturas.map((f,i)=>(
+            <section 
+              className="flex w-full items-center h-16 rounded hover:shadow  hover:bg-white/40 hover:text-white justify-between pr-2 group cursor-pointer"
+              key={i}
+            >
+              <article className=" flex justify-start ">
+
+                <FileZipIcon className="w-12 "/>
+                <article className=" flex flex-col">
+                  <span className="flex-1 whitespace-nowrap text-xs text-amber-200/75 font-extrabold">
+                  {f.numero}
+                  </span>
+                  <span className=" text-xs">
+                    {`RUC: ${f.RUC}`}
+                  </span>
+                  <span className="  flex text-xs"> 
+                    <CheckIcon className="w-3 text-[#4caf50]"/>
+                   {`\u00A0 ${f.fecha}`}
+                  </span>
+                </article>
+              </article>
+              <span className="inline-flex items-center  py-0.5 ms-3 text-xs font-medium rounded bg-gray-700 max-w-[90px] px-2 w-full justify-between overflow-hidden">
+                  <span>
+                    {`s/`}
+                  </span>
+                  <span>
+                    4550400.00
+                  </span>                    
               </span>
-              <span className="inline-flex items-center justify-center px-2 py-0.5 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
-                {f.monto}
-              </span>
-          </a>
+            </section>
         ))}      
       </div>
     </div>  
